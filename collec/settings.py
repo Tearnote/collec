900dacb2273a093932ea99b2daccef1167e00c4f
Tearnote <marach5@gmail.com>
2023-03-22 16:02:32 +0000

    Add landing page to index route

--- collec/settings.py
+++ collec/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "vendor",
+]
+
+STATIC_ROOT = BASE_DIR / "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1', ]
