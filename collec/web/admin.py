9ec101c9d529905098e951f767ca3a2749e3369b
Tearnote <marach5@gmail.com>
2023-03-27 14:16:34 +0100

    Register models on the admin page

--- collec/web/admin.py
+++ collec/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Settings)
+admin.site.register(models.Item)
+admin.site.register(models.Tag)
+admin.site.register(models.BookDetails)
+admin.site.register(models.MovieDetails)
+admin.site.register(models.VideogameDetails)
