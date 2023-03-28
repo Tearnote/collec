d1209d194b35083e4eaad599f3e78981a8f918e5
Tearnote <marach5@gmail.com>
2023-03-28 12:17:59 +0100

    Introduce dashboard template with books list

--- collec/web/views.py
+++ collec/web/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+
+from collec.web.models import Item
 
 
 def index(request):
     return render(request, 'web/index.html')
+
+
+def dashboard(request, **kwargs):
+    username = kwargs['username']
+    user = get_object_or_404(User, username=username)
+    books = Item.objects.filter(user=user, type='BK')
+    context = {
+        'user': user,
+        'books': books,
+    }
+    return render(request, 'web/dashboard.html', context)
