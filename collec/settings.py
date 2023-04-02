f0d401fc4a15e98a48e9ede00385d4825c81522b
Tearnote <marach5@gmail.com>
2023-04-02 11:42:16 +0100

    Add allauth to project

--- collec/settings.py
+++ collec/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
     'crispy_forms',
     'crispy_bootstrap5',
     'collec.web',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
